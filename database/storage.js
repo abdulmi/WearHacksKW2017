5e611acd04d0c4765a737838dec889599ed77bd6
Dongoug <dougouk@gmail.com>
2017-03-25 19:13:35 -0400

    Working prog

--- database/storage.js
+++ database/storage.js
@@ -1,16 +1,15 @@
-var firebase = require('firebase')
-var config = {
-   apiKey: "AIzaSyCVMbpAX0ocPFzff3hIoyoJ909w6CkFB_Q",
-   authDomain: "wearhackskw2017-51c15.firebaseapp.com",
-   databaseURL: "https://wearhackskw2017-51c15.firebaseio.com",
-   storageBucket: "wearhackskw2017-51c15.appspot.com",
-   messagingSenderId: "453839360783"
- };
-firebase.initializeApp(config);
+// var config = {
+//    apiKey: "AIzaSyCVMbpAX0ocPFzff3hIoyoJ909w6CkFB_Q",
+//    authDomain: "wearhackskw2017-51c15.firebaseapp.com",
+//    databaseURL: "https://wearhackskw2017-51c15.firebaseio.com",
+//    storageBucket: "wearhackskw2017-51c15.appspot.com",
+//    messagingSenderId: "453839360783"
+//  };
+// firebase.initializeApp(config);
 var Firebase = firebase.database().ref();
 var patients = firebase.database().ref('patients');
 var perscriptions = firebase.database().ref('perscriptions');
-var twilio = require('../texting/text')
+// var twilio = require('../texting/text')
 
 
 
@@ -164,14 +163,14 @@ function getAllPerscriptionsSnap(callback){
     });
 }
 
-module.exports = {
-  getAllPerscriptions:getAllPerscriptions,
-  getEpochPerscription:getEpochPerscription,
-  createPerscription:createPerscription,
-  schedule:schedule,
-  sendMessage: sendMessage,
-  getAllPerscriptionsSnap: getAllPerscriptionsSnap
-}
+// module.exports = {
+//   getAllPerscriptions:getAllPerscriptions,
+//   getEpochPerscription:getEpochPerscription,
+//   createPerscription:createPerscription,
+//   schedule:schedule,
+//   sendMessage: sendMessage,
+//   getAllPerscriptionsSnap: getAllPerscriptionsSnap
+// }
 
 
 function getAllPatients2(callback){
@@ -209,4 +208,3 @@ function sendMessage(perscription){
         });
     });
 }
-
