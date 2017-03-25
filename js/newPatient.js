5ac7f7d718d52194440fdf3a6fe38e5f3fdc3292
Dongoug <dougouk@gmail.com>
2017-03-25 04:00:52 -0400

    Revert "Revert "Merge branch 'web' into database""
    This reverts commit 0e282216c1fc99b9e7a6e588b71e42a76b41ebf6.

---
+++ js/newPatient.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#btnAddSubscription").on('click', function(events) {
+    $("#prescription-container").append('<h2>Add Prescription</h2> <div id="prescription-container">      <div class="prescription-section">      <input class="modal-subButton" id="btnRemovePrescription" type="image" src="res/del_sm.png"/><form><label for="inputlg">Prescription Name</label> <input class="form-control input-lg" type="text"/>    </form>    <form>      <label for="inputlg">Dosage</label>      <input class="form-control input-lg" type="text"/>    </form>    <form>      <label for="inputlg">Details</label>      <input class="form-control input-lg" type="text"/>    </form> </div> </div>');
+      console.log("added");
+  });
+
+  // $("#prescription-container").on('click', '#btnRemovePrescription', function(events) {
+  //   $(this).closest('div').remove();
+  //   console.log("removed");
+  // });
+})
