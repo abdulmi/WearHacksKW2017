65d876701293466b0f748e48da7a1a2d5d9661c4
Veljko Dimic <veljkod122@gmail.com>
2017-03-25 05:05:37 -0400

    Added functionality for Epoch time

--- database/storage.js
+++ database/storage.js
@@ -6,20 +6,45 @@ var perscriptions = new Firebase("https://wearhackskw2017.firebaseio.com/perscri
 
 PerscriptionID = 0;
 
-function schedule(interval, duration, time){
+function schedule(frequency, duration, time){
     var Schedule = {
-        Interval: interval,
+        Frequency: frequency,
         Duration: duration,
-        Start: time(8,0),
+        Interval: 1440, //Default 24 hours (1/day)
+        Start: 480,   //Minutes
     }
     if (time){
-        Schedule.Start = time;
+        Schedule.Start = time;  //Minutes
     }
     else{
-        Schedule.Start = time(8,0);
-        Schedule.Count = Math.ceil(24/interval.Hour)-1;
+        Schedule.Start = 480;
+        Schedule.duration -= Math.ceil(frequency/2);    //This will assume half of the pills have been taken on the day perscribed
     }
+    Schedule.Interval = Math.floor((24*60-Schedule.start)/(frequency-1));
+
+    var s = Schedule.Start*60*1000;
+    var d = new Date();
+    d = d.getTime();
+    d += 86400000;
+    d = d - d.getMilliseconds() - d.getSeconds()*1000 - d.getMinutes()*60*1000 - d.getHours()*60*60*1000;
+    Schedule.Start = s + d; //In theory, sets start time to epoch time
+
     return Schedule;
+}
+
+//Returns the Epoch time of the time it is looking out for
+function getEpochPerscription(perscription, callback){
+    var time = perscription.Schedule.Start;
+    var c = perscription.Count;
+    var f = perscription.Schedule.Frequency;
+    var i = perscription.Schedule.Interval;
+    time = time + Math.floor(c/f) + i*(c%f);
+    if (time){
+        callback(time);
+    }
+    else{
+        callback();
+    }
 }
 
 function createPatient(id, name, number, age, method){
