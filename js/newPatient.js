cfd0fe989d52123fe4e2a4b791240c1ebef8c20a
Dongoug <dougouk@gmail.com>
2017-03-25 07:57:11 -0400

    Dynamically adding prescription entries

--- js/newPatient.js
+++ js/newPatient.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
   $("#btnAddSubscription").on('click', function(events) {
     $("#prescription-container").append('<div id="prescription-container">      <div class="prescription-section">   <h2>Add Prescription</h2>   <input class="modal-subButton" id="btnRemovePrescription" type="image" src="res/minus.png"/><form><label for="inputlg">Prescription Name</label> <input class="form-control input-lg" type="text"/>    </form>    <form>      <label for="inputlg">Dosage</label>      <input class="form-control input-lg" type="text"/>    </form>    <form>      <label for="inputlg">Details</label>      <input class="form-control input-lg" type="text"/>    </form> </div> </div>');
+
+    var textDosage = '<label for="inputlg">Dosage schedule</label>    <label for="input-sm">Frequency (per day)</label>    <input class="form-control input-lg" type="text"/>    <label for="input-sm">Start time</label> <select class="hour input-lg" style="width: auto;">';
+
+    for(var i = 0; i <= 9; i++){
+      textDosage += "<option value=" + i + ">0" + i + "</option>";
+    }
+    for(var i = 10; i <=23; i++){
+      textDosage += "<option value=" + i + ">" + i + "</option>";
+    }
+    textDosage += "</select>";
+    textDosage += ":";
+    textDosage += '<select class="minutes input-lg" style="width: auto;">'
+    for(var i = 0; i <= 9; i++){
+      textDosage += "<option value=" + i + ">0" + i + "</option>";
+    }
+    for(var i = 10; i <=59; i++){
+      textDosage += "<option value=" + i + ">" + i + "</option>";
+    }
+    textDosage += "</select>"
+    textDosage += "</form>"
+    $('#prescription-container').append(textDosage);
+
       console.log("added");
   });
 
